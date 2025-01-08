b92e83126365b08f357fc13b5408c0645aab5512
yuunha <h000414@naver.com>
2025-01-08 14:41:14 +0900

    settings

---
+++ src/main/java/com/ll/jpa_250108/Jpa250108Application.java
@@ -0,0 +1,13 @@
+package com.ll.jpa_250108;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa250108Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa250108Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jpa_250108/Jpa250108ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpa_250108;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa250108ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
