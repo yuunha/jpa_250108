bbb35ff9a3f210f4e894e2b99a9b8712ad60ea0e
yuunha <h000414@naver.com>
2025-01-08 17:31:46 +0900

    쿼리와 결과 출력

--- src/main/java/com/ll/jpa/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jpa/global/initData/BaseInitData.java
@@ -19,11 +19,9 @@ public class BaseInitData {
             if (postService.count() > 0) return; // 게시물 0개이상이면, 아래코드 실행X
 
             Post post1= postService.write("title1","content1");
-            System.out.println(post1.getId()+"번 글이 생성됨.");
             Post post2= postService.write("title2","content2");
-            System.out.println(post2.getId()+"번 글이 생성됨.");
             Post post3= postService.write("title3","content3");
-            System.out.println(post3.getId()+"번 글이 생성됨.");
+
         };
     }
 
